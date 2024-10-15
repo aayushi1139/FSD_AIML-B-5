9628043b361b93921f9ef229c72364452176784c
aayushi1139 <lalitahaj@gmail.com>
2024-10-15 10:29:40 +0530

    jss

---
+++ JSSWork/index.js
@@ -0,0 +1,16 @@
+// console.log("Hello JS");
+// // var a=12;
+// const a=12;
+// if(a>10){
+//    let b=23;
+//    a=40;
+//     console.log("a="+a);
+// }
+// console.log("hi, a="+a);
+// console.log("hi, a="+b);
+function fun(a,b){
+    console.log("hi");
+    return a+b;
+}
+let data=fun(23,4);
+console.log(data);
